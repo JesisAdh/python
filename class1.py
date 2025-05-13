b1421081e16075fc71f961cf93e9cce4b0a0ab26
JesisAdh <jesisadhikari75@gmail.com>
2025-05-13 09:02:58 +0545

    class1.py

---
+++ class1.py
@@ -0,0 +1,12 @@
+# to instal the package 
+# python.exe -m pip install pandas==<version> 
+
+# 9849808628
+#  to create variables  
+# no number in the firt chArater
+number = 123
+name = "abc"
+print(number)
+#  snake case == double word ko underscore  jeis_adh
+# camel case == alternative captial  jesisAdh
+
